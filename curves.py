8b2bcbc215b08aec3fd461bc1c1b529341d7915d
cabo40 <cfso100@gmail.com>
2017-01-18 22:43:57 -0600

    Slow step to coder but great time invested.... chill

--- curves.py
+++ curves.py
@@ -1,6 +1,11 @@
 import sqlite3
+import json
+
 sqlite_file = ".SSKT_curves.db"
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS Curves
+             (name text, currency text, vector text)''')
+
 conn.commit()
 conn.close()
